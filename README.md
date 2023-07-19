2f78bbe009b3f61e5b3fcab2dec97ce2c5829d54
vlad <vlad@vlads-MBP.netis.cc>
2023-07-19 18:29:21 +0200

    db backup and replace functionality

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Read the docs:
+## Read the docs:
 
 Setup doctl:
 https://docs.digitalocean.com/products/functions/how-to/create-functions/
@@ -8,6 +8,22 @@ https://docs.digitalocean.com/products/functions/reference/runtimes/node-js/
 
 But basically after everything:
 
- 1. add ZIP to the zips.json
+ ```doctl serverless deploy pgsql-zip-replace```
 
- 2. doctl serverless deploy pgsql-zip-replace
+
+ ## HOW:
+ Looks up JSON in local storage to return AREA, if doesn't find queries DB and returns from there, then stores resutl in local storage for future use.
+ 
+
+ ## Changes:
+ - To replace single AREA in the local storage:
+ Make POST request to function url with following body:
+ ```
+ {
+    "digit": "34500",
+    "replace": true,
+    "area": "NEW AREA"
+}
+```
+
+- To make big changes just redeploy with empty or updated zips.json file.
--- pgsql-zip-replace/packages/wefix/twillio-zip-replace/index.js
+++ pgsql-zip-replace/packages/wefix/twillio-zip-replace/index.js
@@ -1,18 +1,88 @@
-import { readFile } from 'fs/promises';
+import { readFile, writeFile } from 'fs/promises';
+import pg from 'pg';
 
 export async function main(event) {
 
-    let data = JSON.parse(await readFile("zips.json", "utf8"));
+  let data = JSON.parse(await readFile("zips.json", "utf8"));
 
-    let zip = event.digit;
+  let zip = event.digit;
 
-    let response = {"area": data.ZIPS[zip]}
+  if (zip in data.ZIPS) {
+    if (event.replace && event.area) {
 
-    return {
+      let old = data.ZIPS[zip];
+      data.ZIPS[zip] = event.area
+
+      let json = JSON.stringify(data);
+      await writeFile('zips.json', json, 'utf8');
+
+      let response = { "Succes": `Changed ${zip} from ${old} to ${event.area}` }
+
+      return {
         body: response,
         statusCode: 200,
         headers: {
           'Content-Type': 'application/json'
         }
       }
+
+    }
+    return {
+      body: {"Error": `Local storage doesn't have records for ${zip}`},
+      statusCode: 404,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+  }
+
+  if (zip in data.ZIPS) {
+    let response = { "area": data.ZIPS[zip] }
+    return {
+      body: response,
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  }
+
+  const client = new pg.Client({
+    user: 'wefix_pg_db',
+    host: '142.93.246.166',
+    database: 'wefix_database',
+    password: 'Gvxt*ScDY5',
+    port: 5432,
+  })
+
+  await client.connect()
+
+  const res = await client.query('SELECT area FROM "ZIP" z WHERE zip = $1', [zip])
+
+  if (!res.rows[0]) {
+
+    let response = { "area": "Invalid or unknown ZIP" }
+    return {
+      body: response,
+      statusCode: 400,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+  }
+
+  data.ZIPS[zip] = res.rows[0]["area"]
+
+  let json = JSON.stringify(data);
+  await writeFile('zips.json', json, 'utf8');
+
+  return {
+    body: res.rows[0],
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
 }
